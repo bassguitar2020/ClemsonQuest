894e2a2b12419dd0fe3ee73defdb0fb844ed2b8f
Johan Z. <150753578+psychedelic-theory@users.noreply.github.com>
2025-12-09 23:17:33 -0500

    Small changes for testing

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 function requireEnv(name: string) {
   const value = process.env[name];
@@ -18,11 +19,12 @@ const firebaseConfig = {
   storageBucket: requireEnv('EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET'),
   messagingSenderId: requireEnv('EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'),
   appId: requireEnv('EXPO_PUBLIC_FIREBASE_APP_ID'),
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  measurementId: requireEnv('EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID'),
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
